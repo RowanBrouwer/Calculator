ff2b83e27f3a5492c3979e5cf6280afa96d9ae8e
rowan brouwer <brouwerrowan@gmail.com>
2020-03-12 12:25:14 +0100

    update

---
+++ Bibliotheek/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Bibliotheek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.check = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.telaat = new System.Windows.Forms.Label();
+            this.tld = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.prijs = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(161, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(297, 29);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(161, 145);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(297, 29);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Uitgeleend op :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingeleverd op :";
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(315, 180);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(143, 64);
+            this.check.TabIndex = 4;
+            this.check.Text = "Check";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Te laat :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dagen te laat :";
+            // 
+            // telaat
+            // 
+            this.telaat.AutoSize = true;
+            this.telaat.Location = new System.Drawing.Point(399, 266);
+            this.telaat.Name = "telaat";
+            this.telaat.Size = new System.Drawing.Size(0, 25);
+            this.telaat.TabIndex = 7;
+            // 
+            // tld
+            // 
+            this.tld.AutoSize = true;
+            this.tld.Location = new System.Drawing.Point(608, 266);
+            this.tld.Name = "tld";
+            this.tld.Size = new System.Drawing.Size(0, 25);
+            this.tld.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(310, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Prijs Boete :";
+            // 
+            // prijs
+            // 
+            this.prijs.AutoSize = true;
+            this.prijs.Location = new System.Drawing.Point(432, 369);
+            this.prijs.Name = "prijs";
+            this.prijs.Size = new System.Drawing.Size(0, 25);
+            this.prijs.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 199);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 29);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Romans";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 234);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(159, 29);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Studieboeken";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.prijs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tld);
+            this.Controls.Add(this.telaat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label telaat;
+        private System.Windows.Forms.Label tld;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label prijs;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Bibliotheek/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bibliotheek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bereken()
+        {
+            telaat.Text = "";
+            tld.Text = "";
+            prijs.Text = "";
+            DateTime uitgeleend = dateTimePicker1.Value.Date;
+            DateTime ingeleverd = dateTimePicker2.Value.Date;
+            TimeSpan dagen = ingeleverd - uitgeleend;
+            int d = dagen.Days;
+            if (checkBox1.Checked == true && checkBox2.Checked == true)
+            {
+                prijs.Text = "Kies 1 soort boek dat word ingeleverd";
+            }
+            else if (checkBox1.Checked == true)
+            {
+                if (d > 21)
+                {
+                    telaat.Text = "Ja";
+                    double boete = (d - 21) * 0.25;
+                    System.Convert.ToDecimal(boete);
+                    prijs.Text = boete.ToString("€ ##.##");
+                    tld.Text = (d - 21).ToString();
+                    checkBox1.Checked = false;
+                }
+                else if (d < 21)
+                {
+                    telaat.Text = "Nee";
+                    checkBox1.Checked = false;
+                }
+            }
+            else if (checkBox2.Checked == true)
+            {
+                if (d > 30)
+                {
+                    int week = 7;
+                    double wekenlaat = (d - 30) / week;
+                    double boete = wekenlaat * 1;
+                    System.Convert.ToDecimal(boete);
+                    prijs.Text = boete.ToString("€ ##.##");
+                    tld.Text = (d - 30).ToString();
+                    checkBox2.Checked = false;
+                }
+                else if (d < 30)
+                {
+                    telaat.Text = "Nee";
+                    checkBox1.Checked = false;
+                }
+            }
+            else if (checkBox1.Checked == false && checkBox2.Checked == false)
+            {
+                prijs.Text = "Kies het soort boek dat word ingeleverd";
+            }
+
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            Bereken();
+        }
+
+    }
+}
