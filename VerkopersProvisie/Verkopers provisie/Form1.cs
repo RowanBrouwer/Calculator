e7e4decc18fccdbf3ddb969bfc6e703aa4e96fb9
rowan brouwer <brouwerrowan@gmail.com>
2020-03-12 10:01:01 +0100

    change

--- VerkopersProvisie/Verkopers provisie/Form1.cs
+++ VerkopersProvisie/Verkopers provisie/Form1.cs
@@ -16,67 +16,53 @@ namespace Verkopers_provisie
         {
             InitializeComponent();
         }
-        private void bereken()
+        private void Bereken()
         {
-
-            var i1 = m1.Text;
-            var i2 = m2.Text;
-            var i3 = m3.Text;
-            var i4 = m4.Text;
-            var i5 = m5.Text;
-            var i6 = m6.Text;
-            var i7 = m7.Text;
-            var i8 = m8.Text;
-            var i9 = m9.Text;
-            var i10 = m10.Text;
-            var i11 = m11.Text;
-            var i12 = m12.Text;
-            int p1 = 0;
-            int p2 = 0;
-            int p3 = 0;
-            int p4 = 0;
-            int p5 = 0;
-            int p6 = 0;
-            int p7 = 0;
-            int p8 = 0;
-            int p9 = 0;
-            int p10 = 0;
-            int p11 = 0;
-            int p12 = 0;
+            var p1 = 0; var k1 = m1.Text;
+            var p2 = 0; var k2 = m2.Text;
+            var p3 = 0; var k3 = m3.Text;
+            var p4 = 0; var k4 = m4.Text;
+            var p5 = 0; var k5 = m5.Text;
+            var p6 = 0; var k6 = m6.Text;
+            var p7 = 0; var k7 = m7.Text;
+            var p8 = 0; var k8 = m8.Text;
+            var p9 = 0; var k9 = m9.Text;
+            var p10 = 0; var k10 = m10.Text;
+            var p11 = 0; var k11 = m11.Text;
+            var p12 = 0; var k12 = m12.Text;
             try
             {
-                p1 = System.Convert.ToInt32(i1);
-                p2 = System.Convert.ToInt32(i2);
-                p3 = System.Convert.ToInt32(i3);
-                p4 = System.Convert.ToInt32(i4);
-                p5 = System.Convert.ToInt32(i5);
-                p6 = System.Convert.ToInt32(i6);
-                p7 = System.Convert.ToInt32(i7);
-                p8 = System.Convert.ToInt32(i8);
-                p9 = System.Convert.ToInt32(i9);
-                p10 = System.Convert.ToInt32(i10);
-                p11 = System.Convert.ToInt32(i11);
-                p12 = System.Convert.ToInt32(i12);
+                int.TryParse(k1, out p1); int.TryParse(k2, out p2);
+                int.TryParse(k3, out p3); int.TryParse(k4, out p4);
+                int.TryParse(k5, out p5); int.TryParse(k6, out p6);
+                int.TryParse(k7, out p7); int.TryParse(k8, out p8);
+                int.TryParse(k9, out p9); int.TryParse(k10, out p10);
+                int.TryParse(k11, out p11); int.TryParse(k12, out p12);
+
             }
             catch (FormatException)
             {
-                throw;
+                uitkomst.Text = "";
             }
             catch (OverflowException)
             {
                 throw;
             }
-            int V = 1000;
-            double procnt = 0.0;
-            int total = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12;
-            procnt = total / V;
-            if (total < 10000)
+            var V = 1000;
+            var total = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12;
+            var procnt = total / V;
+            if (total == 0)
+            {
+                uitkomst.Text = "0%";
+            }
+            else if (total < 10000)
             {
                 uitkomst.Text = "3%";
             }
             else if (total > 10000 || total < 20000)
             {
-                uitkomst.Text = (procnt * 1.5).ToString("0 procent");
+                string ant = (procnt * 1.5).ToString("00.0");
+                uitkomst.Text = ant+("%");
             }
             else if (total > 20000)
             {
@@ -86,18 +72,14 @@ namespace Verkopers_provisie
                 }
                 else if (procnt < 50)
                 {
-                    uitkomst.Text = (procnt * 2).ToString("0 procent");
+                    string ant = (procnt * 2).ToString("00.0");
+                    uitkomst.Text = ant + ("%");
                 }
             }
-
-            }
-
-            
-        
-       
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            bereken();
+            Bereken();
         }
     }
 }
