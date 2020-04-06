9f98d467a817888a58128f54b446c7676f3c6802
rowan brouwer <brouwerrowan@gmail.com>
2020-04-06 09:25:30 +0200

    Made with an tutorial.

---
+++ BlockGameThing/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
